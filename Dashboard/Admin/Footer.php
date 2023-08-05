746cd1bad154a7a7a955a3ab60666f3c6a8e37f8
Erwin <erwinshrestha17@gmail.com>
2023-08-05 12:31:30 +0545

    Initial commit

---
+++ Dashboard/Admin/Footer.php
@@ -0,0 +1,54 @@
+<!Doctype html>
+<html lang="eng">
+<head>
+
+    <title>Dashboard</title>
+<!--     Fonts and icons     -->
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+<!-- Nucleo Icons -->
+<link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+<link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+<!-- Font Awesome Icons -->
+<script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+<!-- Material Icons -->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+<!-- CSS -->
+<link id="pagestyle" href="../Assets/css/material-dashboard.min.css" rel="stylesheet" />
+</head>
+<body>
+
+</body>
+<!-- End Navbar -->
+<div class="container-fluid py-4">
+    <div class="row min-vh-80 h-100">
+        <div class="col-12">
+
+        </div>
+    </div>
+    <footer class="footer pt-5">
+        <div class="container-fluid">
+            <div class="row align-items-center justify-content-lg-between">
+                <div class="col-lg-12">
+                    <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link text-muted" target="_blank">Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link text-muted" target="_blank">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link text-muted" target="_blank">Service</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link pe-0 text-muted" target="_blank">License</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
+<script src="../Assets/js/bootstrap.bundle.min.js"></script>
+<script src="../Assets/js/perfect-scrollbar.min.js"></script>
+<script src="../Assets/js/smooth-scrollbar.min.js"></script>
+</html>
---
+++ Dashboard/Admin/TestIndex.php
@@ -0,0 +1,5 @@
+<html>
+<?php include 'Sidebar.php';?>
+<?php include "Main.php";?>
+<?php include "Footer.php";?>
+</html>
